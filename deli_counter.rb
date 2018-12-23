c7e4296ffa9e232e1e887df64f59dad2d8d535ef
Michael Moravetz <moravemv@gmail.com>
2018-12-23 23:57:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,9 @@
-katz_deli = []
-
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The Line is currently empty."
-
+else
+  katz_deli.each do |custy| 
+    puts "The is is currently" + "#{[custy +1]}. #{custy}"
+  end 
+ end
 end
